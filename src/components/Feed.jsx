e0f193a8cb13955d63feab7972cf2776b9d19501
Sarath Chandra Dhanalakota <dschandra2003@gmail.com>
2025-03-09 11:43:20 +0530

    editing profile

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,34 @@
+import { useDispatch, useSelector } from "react-redux";
+import UserCard from "./UserCard";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import {addFeed} from "../utils/feedSlice";
+import { useEffect } from "react";
 
 const Feed = () => {
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
+
+  const getFeed = async ()=>{
+    if(feed) return;
+    try{
+      const res = await axios.get(BASE_URL+"/feed",{
+        withCredentials: true,
+      });
+      dispatch(addFeed(res?.data?.data));
+    }catch(err){
+      console.error(err);
+    }
+  };
+
+  useEffect(()=>{
+    getFeed();
+  },[]);
+
   return (
-    <div>Feed</div>
+    feed && (<div className="flex justify-center my-10">
+      <UserCard user={feed[0]}/>
+    </div>)
   )
 }
 
